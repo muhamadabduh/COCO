da887963a8df4e7bd4237ded71f1b1d648f73362
Hilarion18 <hilarion_cross@hotmail.com>
2018-11-02 00:40:07 +0700

    tes after google sig in

--- server/app.js
+++ server/app.js
@@ -3,9 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const fileMongo = "mongodb://localhost:27017/CocoWeb"
+const db = mongoose.connection
+mongoose.connect(fileMongo, {useNewUrlParser: true})
+const port = process.env.PORT || 3000
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signinRouter = require('./routes/signIn')
 
 var app = express();
 
@@ -16,11 +24,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gsignin', signinRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +47,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.listen(port, (req,res) => {
+  console.log(`Server is running on port: ${port} `);
+  
+})
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var {gSignIn, createAcc} =  require('../controllers/userController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
