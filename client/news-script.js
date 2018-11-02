57e548eb965c01c0b6e0910b66d4077d89ec3368
Hilarion18 <hilarion_cross@hotmail.com>
2018-11-02 13:25:26 +0700

    api news updated and distributed to html

--- client/news-script.js
+++ client/news-script.js
@@ -1,18 +1,22 @@
 $(".list-news").click(function(){ 
+    $("#news").empty()
     $.ajax({
         method: "GET",
         url: "http://localhost:4000/news/all-source",
     })
     .done(function(data){
+        let count = 0
         for(let i = 0;i < data.sources.length;i++){
-            $("#news").append(`<tr>
-                <td><strong>${data.sources[i].name}</strong></td>
-                <td>${data.sources[i].country}</td>
-                <td>${data.sources[i].description}</td>
+            if ( count < 5) {
+                $("#news").append(`
+                <tr>
+                    <td><strong>${data.sources[i].name}</strong></td>
+                    <td>${data.sources[i].country}</td>
+                    <td>${data.sources[i].description}</td>
                 </tr>`)
-            
+            }
+            count++
         }
-        
     })
     .fail(function(){
         console.log('error')
@@ -20,20 +24,22 @@ $(".list-news").click(function(){
 })
 
 $(".list-tv").click(function(){ 
+    $("#news").empty()
     $.ajax({
         method: "GET",
         url: "http://localhost:4000/news/all-source",
     })
     .done(function(data){
+        let count = 0
         for(let i = 0;i < data.sources.length;i++){
-            $("#news").append(`<tr>
-                <td><strong>${data.sources[i].name}</strong></td>
-                <td>${data.sources[i].country}</td>
-                <td>${data.sources[i].description}</td>
-                </tr>`)
-            
+            if ( count < 5) {
+                $("#news").append(`<tr>
+                    <td><strong>${data.sources[i].name}</strong></td>
+                    <td>${data.sources[i].country}</td>
+                    <td>${data.sources[i].description}</td>
+                    </tr>`)
+            }
         }
-        
     })
     .fail(function(){
         console.log('error')
