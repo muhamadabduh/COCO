516888dd6bd7e2d72a92ee8774f1614291cf0c29
hafrizresa <hafrizresa@gmail.com>
2018-11-02 13:48:39 +0700

    add Twitch

--- server/controllers/twitchController.js
+++ server/controllers/twitchController.js
@@ -1,19 +1,9 @@
 const request =require('request')
 class Controller {
     static getHome(req,res){
-        // var options = {
-        //     url: 'https://api.twitch.tv/helix/streams?game_id=33214',
-        //     headers: {
-        //         'User-Agent': 'request',
-        //         'Client-ID' : 'ie8a19qcy21809mro01va3ccx3ni4u'
-        //       }
-        // }
-        // request(options, function(err,data){
-        //     res.send(JSON.parse(data.body))
-        // })
 
         var options = {
-            url:'https://api.twitch.tv/kraken/streams/?game=Overwatch',
+            url:'https://api.twitch.tv/kraken/streams/featured',
             headers: {
                 'User-Agent': 'request',
                 'Accept': 'application/vnd.twitchtv.v5+json',
@@ -21,6 +11,39 @@ class Controller {
               }
         }
         request(options, function(err,data){
+            res.send(JSON.parse(data.body))
+        })
+    }
+
+    static search (req, res){
+        console.log(req.params.game)
+        var joinedUrl = 'https://api.twitch.tv/kraken/streams/?game='+ req.params.game
+        let options= {
+            url: joinedUrl,
+            headers: {
+                'User-Agent': 'request',
+                'Accept': 'application/vnd.twitchtv.v5+json',
+                'Client-ID' : 'ie8a19qcy21809mro01va3ccx3ni4u'
+              }
+        }
+
+        request(options, function(err, data){
+            res.send(JSON.parse(data.body))
+        })
+    }
+
+    static topGame(req,res){
+        let options= {
+            url: 'https://api.twitch.tv/kraken/games/top',
+            headers: {
+                'User-Agent': 'request',
+                'Accept': 'application/vnd.twitchtv.v5+json',
+                'Client-ID' : 'ie8a19qcy21809mro01va3ccx3ni4u'
+         
+            }
+        }
+        request(options,function(err,data){
+         
             res.send(JSON.parse(data.body))
         })
     }
